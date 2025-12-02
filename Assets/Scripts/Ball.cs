2abbf6b5409e6cdd776e49a19e2c173ddd3ed096
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-12-02 18:21:58 +0000

    Add ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float speed = 300f;
+    Vector2 Velocity;
+
+    void Start()
+    {
+        Velocity.x = Random.Range(-1f, 1f);
+        Velocity.y = 1f;
+        rb2D.AddForce(Velocity * speed);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,23 @@ public class Player : MonoBehaviour
     private float inputValue;
     public float moveSpeed = 25f;
     private Vector2 moveDirection;
-    void Start()
-    {
-        
-    }
+    
 
-   
     void Update()
     {
-        
+        ///pulsa "a" o flecha izquierda = -1//pulsa "d" o flecha derecha = 1//no pulsa nada = 0
+        inputValue = Input.GetAxis("Horizontal");
+        if (inputValue == 1)
+        {             moveDirection = Vector2.right;
+        }
+        else if (inputValue == -1)
+        {
+            moveDirection = Vector2.left;
+        }
+        else
+        {
+            moveDirection = Vector2.zero;
+        }
+        rb.AddForce(moveDirection * moveSpeed * Time.deltaTime * 100);
     }
 }
