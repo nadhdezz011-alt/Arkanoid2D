b4e3cd86a8bd123c85c4da84113c7abb32cb9df5
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-12-02 17:15:12 +0000

    Add script player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float inputValue;
+    public float moveSpeed = 25f;
+    private Vector2 moveDirection;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
